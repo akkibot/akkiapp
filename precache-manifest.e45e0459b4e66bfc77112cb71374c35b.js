9539df9684677d3a33ce0994d2f15d689b80ae3f
ankit.vishnoi <ankit.vishnoi@servicenow.com>
2020-06-20 23:59:08 +0530

    Updates

---
+++ precache-manifest.e45e0459b4e66bfc77112cb71374c35b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5a0a315e89f96b4e24adfe80f409ba",
+    "url": "/akkiapp/index.html"
+  },
+  {
+    "revision": "3acba93040435b247338",
+    "url": "/akkiapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2601a0dc489ca66f02f",
+    "url": "/akkiapp/static/js/2.868a313c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/akkiapp/static/js/2.868a313c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acba93040435b247338",
+    "url": "/akkiapp/static/js/main.10dd5161.chunk.js"
+  },
+  {
+    "revision": "06c634bef19700403bfc",
+    "url": "/akkiapp/static/js/runtime-main.0ae67480.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/akkiapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
